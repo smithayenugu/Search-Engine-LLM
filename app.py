67477ad1dccf489bf360c1c3fb0f515319fa5d42
smithayenugu <156659498+smithayenugu@users.noreply.github.com>
2024-09-05 17:20:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain.agents import initialize_agent,AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
+##
 
 api_wrapper_wiki=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=250)
 wiki=WikipediaQueryRun(api_wrapper=api_wrapper_wiki)
